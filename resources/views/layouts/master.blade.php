d8667368f8330e57e5d2c5aeaabd7c7094bb9f64
guybarnahum <guy@barnahum.com>
2015-06-30 12:23:24 -0700

    New favicons

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -40,24 +40,42 @@
 
         <!-- fav icons -->
 
-        <link rel="icon" type="image/x-icon" href="{{ asset('assets/images/favicons/favicon.ico') }}" >
-        <link rel="icon" type="image/png"    href="{{ asset('assets/images/favicons/favicon.png') }}" >
+<link rel="apple-touch-icon" sizes="57x57" href="/assets/images/favicons/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/assets/images/favicons/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/assets/images/favicons/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/assets/images/favicons/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/assets/images/favicons/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/assets/images/favicons/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/assets/images/favicons/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/assets/images/favicons/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/assets/images/favicons/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/assets/images/favicons/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/assets/images/favicons/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicons/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/assets/images/favicons/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+        <link rel="shortcut icon" type="image/x-icon" href="/assets/images/favicons/favicon.ico" >
+        <link rel="shortcut icon" type="image/png"    href="/assets/images/favicons/favicon.png" >
 
         <!-- iPhone 4 Retina display: -->
         <link rel="apple-touch-icon-precomposed" sizes="114x114"
-            href="{{ asset('assets/images/favicons/apple-touch-icon-114x114-precomposed.png') }}" >
+            href="/assets/images/favicons/apple-icon-114x114.png" >
 
         <!-- iPad: -->
         <link rel="apple-touch-icon-precomposed" sizes="72x72"
-            href="{{ asset('assets/images/favicons/apple-touch-icon-72x72-precomposed.png') }}" >
+            href="/assets/images/favicons/apple-icon-72x72.png" >
 
         <!-- iPhone: -->
         <link rel="apple-touch-icon-precomposed"
-            href="{{ asset('assets/images/favicons/apple-touch-icon-60x60-precomposed.png') }}" >
+            href="/assets/images/favicons/apple-icon-60x60.png" >
 
         <!-- css -->
 
-        <link href="{{ asset('http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,400italic') }}" rel='stylesheet'>
+        <link href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,400italic" rel='stylesheet'>
 
         <link href="{{ asset('assets/css/bootstrap.min.css')           }}" rel='stylesheet'>
         <link href="{{ asset('assets/css/theme.min.css')               }}" rel='stylesheet'>
